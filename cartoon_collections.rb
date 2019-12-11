516e46db06ea04243f84e9002ab6c2e10ecaef63
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 20:33:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,9 +8,7 @@ def summon_captain_planet(array)
 end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
